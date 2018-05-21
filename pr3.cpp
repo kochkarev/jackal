21fed7290db8cead241cbd01d5d75cb831025cea
Alexey Kochkarev <alexey.kochkarev@gmail.com>
2018-05-21 20:42:58 +0300

    GameBoard::card_allowed_pos() added

--- pr3.cpp
+++ pr3.cpp
@@ -22,7 +22,30 @@ private:
 };
 
 void GameBoard::generate_board() {
-	
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		for (int j = 0; j < BOARD_SIZE; j++) {
+			if (card_allowed_pos(i ,j)) {
+				// generate and put card
+			}
+		}
+	}
+	// put ships
+}
+
+bool GameBoard::card_allowed_pos(int row, int col) {
+	if (row == 0 || row == BOARD_SIZE || col == 0 || col == BOARD_SIZE) {
+		return false;
+	} else if (row == 1) {
+		if (col == 1 || col == BOARD_SIZE - 1) {
+			return false;
+		}
+	} else if (row == BOARD_SIZE - 1) {
+		if (col == 1 || col == BOARD_SIZE - 1) {
+			return false;
+		}
+	} else {
+		return true;
+	}
 }
 
 /*
