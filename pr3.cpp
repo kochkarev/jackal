5b973ef05a95aeaae496c6b55da28075f31209f5
Alexey Kochkarev <alexey.kochkarev@gmail.com>
2018-05-21 21:06:04 +0300

    GameBoard::fill_cards_map() added

--- pr3.cpp
+++ pr3.cpp
@@ -22,6 +22,7 @@ private:
 };
 
 void GameBoard::generate_board() {
+	std::map<CardType, int> left_cards = fill_cards_map();
 	for (int i = 0; i < BOARD_SIZE; i++) {
 		for (int j = 0; j < BOARD_SIZE; j++) {
 			if (card_allowed_pos(i ,j)) {
@@ -30,6 +31,22 @@ void GameBoard::generate_board() {
 		}
 	}
 	// put ships
+}
+
+auto GameBoard::fill_cards_map() {
+	std::map<CardType, int> left_cards;
+	left_cards.insert(std::make_pair(EMPTY, CARDS_EMPT));
+	left_cards.insert(std::make_pair(FORTRESS, CARDS_FRT));
+	left_cards.insert(std::make_pair(CHEST1, CARDS_CH1));
+	left_cards.insert(std::make_pair(CHEST2, CARDS_CH2));
+	left_cards.insert(std::make_pair(CHEST3, CARDS_CH3));
+	left_cards.insert(std::make_pair(TREASURE, CARDS_TRS));
+	left_cards.insert(std::make_pair(ARRUP, CARDS_ARRUP));
+	left_cards.insert(std::make_pair(ARRDOWN, CARDS_ARRDOWN));
+	left_cards.insert(std::make_pair(ARRRIGHT, CARDS_ARRRIGHT));
+	left_cards.insert(std::make_pair(ARRLEFT, CARDS_ARRLEFT));
+	left_cards.insert(std::make_pair(MANEATER, CARDS_MEA));
+	return left_cards;
 }
 
 bool GameBoard::card_allowed_pos(int row, int col) {
