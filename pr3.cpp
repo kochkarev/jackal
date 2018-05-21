8309fe0def104fe668b8ace49a8fe0a760294bb6
Alexey Kochkarev <alexey.kochkarev@gmail.com>
2018-05-22 00:39:36 +0300

    generate_board() and destructor of GameBoard added

--- pr3.cpp
+++ pr3.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 
-
-
 class Player {
 public:
 
@@ -15,22 +13,58 @@ private:
 class GameBoard {
 public:
 
+	~GameBoard(); 
 private:
 	std::vector<std::shared_ptr<Card>> board;
-	void generate_board();
 	int players_cnt;
+	void generate_board(); 
+	std::map<CardType, int> fill_cards_map();
+	bool card_allowed_pos(int row, int col);
 };
+
+void GameBoard::~GameBoard() {
+	for (const auto ptr: board) {
+		delete ptr;
+	}
+}
 
 void GameBoard::generate_board() {
 	std::map<CardType, int> left_cards = fill_cards_map();
 	for (int i = 0; i < BOARD_SIZE; i++) {
 		for (int j = 0; j < BOARD_SIZE; j++) {
 			if (card_allowed_pos(i ,j)) {
-				// generate and put card
+				do {
+					int tmp_card = rand() % CARD_TYPES;
+				} while left_cards[tmp_card] <= 0;
+				left_cards[tmp_card]--;
+				switch (tmp_card) {
+					case EMPTY:
+					case FORTRESS: 
+					case CHEST1: 
+					case CHEST2: 
+					case CHEST3: 
+					case TREASURE:
+						board[i * BOARD_SIZE + j] = std::shared_ptr<Card>(new StandCard(tmp_card, i, j));
+						break;
+					case ARRUP: 
+					case ARRDOWN: 
+					case ARRRIGHT: 
+					case ARRLEFT:  
+					case MANEATER: 
+						board[i * BOARD_SIZE + j] = std::shared_ptr<Card>(new NoStandCard(tmp_card, i, j));
+						break;
+					default:
+						throw "BAD GENERATED NUMBER";
+				}
+			} else {
+				board[i * BOARD_SIZE + j] = std::shared_ptr<Card>(new NoCard(i, j));
 			}
 		}
 	}
-	// put ships
+	board[BOARD_SIZE / 2] = std::shared_ptr<Card>(new ShipCard(PLC_RED, 0, BOARD_SIZE / 2));
+	board[BOARD_SIZE * BOARD_SIZE] = std::shared_ptr<Card>(new ShipCard(PLC_GREEN, BOARD_SIZE / 2, 0));
+	board[BOARD_SIZE * BOARD_SIZE + BOARD_SIZE / 2] = std::shared_ptr<Card>(new ShipCard(PLC_BLUE, BOARD_SIZE, BOARD_SIZE / 2));
+	board[BOARD_SIZE * BOARD_SIZE / 2 + BOARD_SIZE] = std::shared_ptr<Card>(new ShipCard(PLC_YELLOW, BOARD_SIZE, BOARD_SIZE / 2));
 }
 
 auto GameBoard::fill_cards_map() {
